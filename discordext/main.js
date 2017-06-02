4950fc84465ad26912ba3c7cd08d7f20c488d531
kprzybylski88 <krzysztof.mikolaj.przybylski@gmail.com>
2017-06-02 16:23:37 +0200

    Add files via upload

--- discordext/main.js
+++ discordext/main.js
@@ -1,7 +1,7 @@
 //Check if messages are loaded. I somehow was unable to make window.onload work for discord. Better ideas?
 var checkIfLoadedFun = function(){
 	var checkIfLoaded = setInterval(function(){
-		console.log('checking')
+		//console.log('checking')
 		var messagesWrapper = document.getElementsByClassName('scroller messages');
 		if (messagesWrapper.length!=0) {
 			clearInterval(checkIfLoaded);
@@ -22,7 +22,7 @@ var okSoWeReDoingIt = function(messagesWrapper){
 	var channelList = document.getElementsByClassName('channel channel-text');
 	for (var i=0; i<channelList.length; ++i) {
 		channelList[i].addEventListener('click', function(){
-			checkIfLoadedFun(); //this fucks something up with intervals clearing.
+			checkIfLoadedFun(); //this fucks something up with intervals clearing. I countered it and all intervals are cleared in the end but still.
 		});
 	}
 	
@@ -45,7 +45,7 @@ var okSoWeReDoingIt = function(messagesWrapper){
 
 }
 
-//adds a button (need to work on that css to make it look pretty(ier?)). The button itself doesn't do much yet but I am able to pull message text if needed
+//adds a button (need to work on that css to make it look pretty(ier?)).
 var addButton=function(message) {
 	doneButtons=true;
 	var innerMessage = message.getElementsByClassName('message-text')[0];
@@ -59,6 +59,39 @@ var addButton=function(message) {
 	}
 }
 
+//gets the message (now embeded in all html gibberish) and username of person archivibg and channel name
 var archiveMessage=function(buttonC) {
-	console.log(buttonC.parentNode.parentNode.innerHTML);
+	//console.log('y');
+	var allMessageInfo = buttonC.parentNode.parentNode.parentNode.parentNode;
+	var message = joinMessages(allMessageInfo.getElementsByClassName('markup'));
+	var messager = allMessageInfo.getElementsByClassName('user-name')[0].innerHTML;
+	var timestamp = allMessageInfo.getElementsByClassName('timestamp')[0].innerHTML;
+	var archiverUsername = document.getElementsByClassName('account-details')[0].getElementsByClassName('username')[0].innerHTML + cropBetweenStrings(document.getElementsByClassName('account-details')[0].getElementsByClassName('discriminator')[0].innerHTML,'<!--','-->');
+	var channel = document.getElementsByClassName('title-wrap')[0].getElementsByClassName('channel-name')[0].innerHTML;
+	console.log('messager: '+messager+'\ntimestamp: '+timestamp+'\nmessage: '+message+'\n\narchiver: '+archiverUsername+'\n\nchannel: '+channel);
+}
+
+var joinMessages = function(messageFragments) {
+	var messageJoined="";
+	for(var i=0; i<messageFragments.length;++i){
+		messageJoined += messageFragments[i].innerHTML+'\n';
+	}
+	messageJoined = cropBetweenStrings(messageJoined,'<!--','-->')
+	return messageJoined.substring(0,messageJoined.length-1);
+}
+
+var cropBetweenStrings = function(stringToCrop,startString,stopString) {
+	croppedString = stringToCrop;
+	stringStillThere=true;
+	while(stringStillThere) {
+		var positionStart = croppedString.indexOf(startString);
+		var positionStop = croppedString.indexOf(stopString);
+		//console.log(positionStart+" "+positionStop);
+		if (positionStop === -1 || positionStart === -1) {
+			stringStillThere = false;
+		} else {
+			croppedString = croppedString.substring(0,positionStart) + croppedString.substring(positionStop+stopString.length,croppedString.length);
+		}
+	}
+	return croppedString;
 }
