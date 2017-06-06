22c6f3baa03268527b18118c44bcec65b1d4e5f1
kprzybylski88 <krzysztof.mikolaj.przybylski@gmail.com>
2017-06-06 14:57:00 +0200

    Add files via upload

--- discordext/main.js
+++ discordext/main.js
@@ -1,31 +1,23 @@
+var addedChannels=[];
 //Check if messages are loaded. I somehow was unable to make window.onload work for discord. Better ideas?
-var checkIfLoadedFun = function(){
+var checkIfLoadedFun = function(className, fun){
 	var checkIfLoaded = setInterval(function(){
-		//console.log('checking')
-		var messagesWrapper = document.getElementsByClassName('scroller messages');
-		if (messagesWrapper.length!=0) {
+		var classTocheck = document.getElementsByClassName(className);
+		if (classTocheck.length!=0) {
 			clearInterval(checkIfLoaded);
-			okSoWeReDoingIt(messagesWrapper);
-				
-	} else {
-		console.log('nope!');
-	}
-},1000);
+			fun(classTocheck);	
+		} else {
+			console.log('nope!');
+		}
+	},1000);
 }
 
-checkIfLoadedFun();
 
 //iterating through messages and firing addButton function
 var okSoWeReDoingIt = function(messagesWrapper){
-	
-	//somehow I could not add MutationObserver for channel changing (it didn't fire) so I added click listener. Problem is the same needs to be done for unhiding and hiding channels. And for guild change ¯\_(ツ)_/¯
-	var channelList = document.getElementsByClassName('channel channel-text');
-	for (var i=0; i<channelList.length; ++i) {
-		channelList[i].addEventListener('click', function(){
-			checkIfLoadedFun(); //this fucks something up with intervals clearing. I countered it and all intervals are cleared in the end but still.
-		});
-	}
-	
+		
+	//channelChange();
+
 	//initial iteration through messages to add archive button
 	var messages = messagesWrapper[0].children;
 	for (var i=0; i<messages.length; ++i){
@@ -43,6 +35,28 @@ var okSoWeReDoingIt = function(messagesWrapper){
 	});
 	scrollObserver.observe(messagesWrapper[0], { attributes: true, childList: true, characterData: true });
 
+	var channelObserver = new MutationObserver(function(mutations) {
+		mutations.forEach(function(mutation) {
+			for (var i = 0; i < mutation.addedNodes.length; i++) {
+				channelChange();
+			}
+		});
+	});
+		
+		channelObserver.observe(document.getElementsByClassName('scroller guild-channels')[0],{ attributes: true, childList: true, characterData: true });
+}
+
+//somehow I could not add MutationObserver for channel changing (it didn't fire) so I added click listener. Problem is the same needs to be done for unhiding and hiding channels. And for guild change ¯\_(ツ)_/¯
+var channelChange = function() {
+	var channelList = document.getElementsByClassName('channel channel-text');
+	for (var i=0; i<channelList.length; ++i) {
+		if (addedChannels.indexOf(channelList[i]) === -1) {
+			channelList[i].addEventListener('click', function(){
+				checkIfLoadedFun('scroller messages',okSoWeReDoingIt); //this fucks something up with intervals clearing. I countered it and all intervals are cleared in the end but still.
+			});
+			addedChannels.push(channelList[i]);
+		}
+	}
 }
 
 //adds a button (need to work on that css to make it look pretty(ier?)).
@@ -97,3 +111,6 @@ var cropBetweenStrings = function(stringToCrop,startString,stopString) {
 	}
 	return croppedString;
 }
+
+
+checkIfLoadedFun('scroller messages',okSoWeReDoingIt);
